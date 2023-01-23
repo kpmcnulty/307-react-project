fb2067a739a71e873487939fdb03e27d70186c14
kmcnulty <kpmcnulty321@gmail.com>
2023-01-23 12:04:36 -0800

    made table default to start

--- react-frontend/src/MyApp.js
+++ react-frontend/src/MyApp.js
@@ -5,22 +5,7 @@ import axios from 'axios';
 function MyApp() {
     
   const [characters, setCharacters] = useState([
-    {
-      name: 'Charlie',
-      job: 'Janitor',
-    },
-    {
-      name: 'Mac',
-      job: 'Bouncer',
-    },
-    {
-      name: 'Dee',
-      job: 'Aspring actress',
-    },
-    {
-      name: 'Dennis',
-      job: 'Bartender',
-    },
+    
 ]);
   function removeOneCharacter (index) {
     const updated = characters.filter((character, i) => {
